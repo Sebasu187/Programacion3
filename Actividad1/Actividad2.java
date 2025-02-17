da4e8695bd56bcbe64aac8971488cf2d71522f9e
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-17 21:18:14 +0000

    creamos los arrays necesariios

--- Actividad1/Actividad2.java
+++ Actividad1/Actividad2.java
@@ -1,12 +1,19 @@
-import java .util.Scanner;
+import java.util.Scanner;
 
 public class Actividad2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Cuántos empleados hay? ");
         int N = scanner.nextInt();
+
+        double[] sueldos = new double[N];
+        double[] asignaciones = new double[N];
+        double[] deducciones = new double[N];
+        double[] netoPagar = new double[N];
+
+        
 
     }
 }
