2ee9990f201457de50f827328df40934fe49fc14
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-17 21:15:33 +0000

    pedimos empleados

---
+++ Actividad1/Actividad2.java
@@ -0,0 +1,12 @@
+import java .util.Scanner;
+
+public class Actividad2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Cuántos empleados hay? ");
+        int N = scanner.nextInt();
+
+    }
+}
