21d96704497c2fb6ad86b648ff9127f12b074760
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-17 19:56:11 +0000

    pedimos los N numeros al usuario creamos el ciclo for dependiendo del tamaño de n y almacenamos

--- Actividad1/Actividad1.java
+++ Actividad1/Actividad1.java
@@ -4,8 +4,14 @@ public class Actividad1 {
         Scanner sc = new Scanner(System.in);
         System.out.println("ingresa el tamaño del arreglo");
         int N = sc.nextInt();
-        int [] Narreglos = new int[N]; //creamos un arreglo del tamaño ingresado en el sc
-        
+        int [] Narreglo = new int[N]; //creamos un arreglo del tamaño ingresado en el sc}
+
+        System.out.println("ingresa los " + N + " numeros del arreglo bro:"); //pedimos los numeros del arreglo
+        for (int i = 0; i < N; i++) { //ciclo para pedir los numeros del arreglo
+            System.out.print("Número " + (i + 1) + ": ");
+            Narreglo[i] = sc.nextInt();
+        }
+
         
     }
 }
