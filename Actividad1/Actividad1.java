1200848aebc5e93294812a351a26b945756bfc46
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-17 19:45:45 +0000

    pedimos y almacenamos el arreglo

--- Actividad1/Actividad1.java
+++ Actividad1/Actividad1.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class Actividad1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa el tamaño del arreglo");
+        int N = sc.nextInt();
+        int [] Narreglos = new int[N]; //creamos un arreglo del tamaño ingresado en el sc
+        
         
     }
 }
