705190009f07e9d761072192a562351875c888d9
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 05:23:24 +0000

    se pide el tamaño del arreglo

---
+++ Actividad1/Actividad9.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Actividad9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        int tamaño = scanner.nextInt();
+    }
+}
