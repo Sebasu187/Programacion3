44185cc7a1d7d107eaa872930f48e18dfada3157
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 00:54:04 +0000

    como son don grupos entonces haremos dos arreglos

---
+++ Actividad1/Actividad3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Actividad3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de personas en el primer grupo: ");
+        int N = scanner.nextInt();
+        System.out.print("Ingrese el número de personas en el segundo grupo: ");
+        int M = scanner.nextInt();
+
+
+        
+    }
+    
+}
