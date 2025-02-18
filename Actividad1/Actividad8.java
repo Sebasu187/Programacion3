ce38f6c0031228b84820c096dfba519cc8bcf3d9
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 05:16:05 +0000

    Recorrer el arreglo para encontrar el número mayor y el menor

--- Actividad1/Actividad8.java
+++ Actividad1/Actividad8.java
@@ -3,5 +3,14 @@ public class Actividad8 {
         int[] valores = {5, 1, 2, 5, 3, 1, 5};
         int maximo = valores[0], minimo = valores[0];
         int contadorMax = 0, contadorMin = 0;
+        for (int i = 1; i < valores.length; i++) {
+            if (valores[i] > maximo) {
+                maximo = valores[i];
+            }
+            if (valores[i] < minimo) {
+                minimo = valores[i];
+            }
+        }
+
     }
 }
