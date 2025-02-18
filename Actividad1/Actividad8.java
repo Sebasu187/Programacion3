f71463d2254d8b72f6d88698fb8a968cc7f030c2
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 05:12:08 +0000

    creamos las variables para el numero mayo y menor y contaodres

---
+++ Actividad1/Actividad8.java
@@ -0,0 +1,7 @@
+public class Actividad8 {
+    public static void main(String[] args) {
+        int[] valores = {5, 1, 2, 5, 3, 1, 5};
+        int maximo = valores[0], minimo = valores[0];
+        int contadorMax = 0, contadorMin = 0;
+    }
+}
