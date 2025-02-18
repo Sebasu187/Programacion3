0f72a81e694e7e83caac6ad9f8335bccf156662a
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 03:03:49 +0000

    .

--- Actividad1/Actividad5.java
+++ Actividad1/Actividad5.java
@@ -10,6 +10,42 @@ public class Actividad5 {
         String[] descripciones = new String[N];    
         double[] totalGastado = new double[N]; 
 
+        System.out.println("\nIngrese los datos de los productos:");
+        for (int i = 0; i < N; i++) {
+            System.out.println("\nProducto " + (i + 1) + ":");
+            System.out.print("Descripción: ");
+            scanner.nextLine(); 
+            descripciones[i] = scanner.nextLine();
+            System.out.print("Precio unitario (PU): ");
+            preciosUnitarios[i] = scanner.nextDouble();
+            System.out.print("Cantidad comprada (CC): ");
+            cantidadesCompradas[i] = scanner.nextInt();
+
+            totalGastado[i] = preciosUnitarios[i] * cantidadesCompradas[i];
+        }
+        double totalGeneral = 0;
+        for (int i = 0; i < N; i++) {
+            totalGeneral += totalGastado[i];
+        }
+
+       
+        int indiceMayorGasto = 0;
+        for (int i = 1; i < N; i++) {
+            if (totalGastado[i] > totalGastado[indiceMayorGasto]) {
+                indiceMayorGasto = i;
+            }
+        }
+
+        
+        System.out.println("\nResultados:");
+        System.out.println("Total general de todas las compras: " + totalGeneral);
+        System.out.println("Producto con mayor gasto:");
+        System.out.println("  Descripción: " + descripciones[indiceMayorGasto]);
+        System.out.println("  Total gastado: " + totalGastado[indiceMayorGasto]);
+
+        
+        scanner.close();
+
         
     }
     
