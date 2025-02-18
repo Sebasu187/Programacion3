1478575ff95b30156211e29ddd2d53cdf828bbce
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 02:19:42 +0000

    pedimos el numero de productos

---
+++ Actividad1/Actividad5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Actividad5 {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("ingrese el numero de productos: ");
+        int N = scanner.nextInt();
+
+        
+    }
+    
+}
