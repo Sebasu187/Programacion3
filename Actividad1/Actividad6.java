9c8db52e9c29cfe2bba00917d54be717c937bdd9
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 04:30:52 +0000

    COMO LA ACTIVIDAD ANTERIORIO CREAMOS LOS ARREGLOS PARA CADA UNO DE LOS DATOS NECESARIOS

--- Actividad1/Actividad6.java
+++ Actividad1/Actividad6.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 public class Actividad6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Ingrese el número de viviendas: ");
         int N = scanner.nextInt();
+
+        double[] alquileres = new double[N];       
+        double[] porcentajesGanancia = new double[N]; 
+        double[] ganancias = new double[N];
+               
 
     }
     
