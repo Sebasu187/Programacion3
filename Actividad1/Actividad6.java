fa142d0741b5a966d1c1c4f2fe399d32ebfaff89
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-18 04:29:34 +0000

    Pedir al usuario el número de viviendas (N)

---
+++ Actividad1/Actividad6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Actividad6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese el número de viviendas: ");
+        int N = scanner.nextInt();
+
+    }
+    
+}
