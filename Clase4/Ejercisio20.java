2f5c7d5d810ba506e65629c6349dc7413b360d2b
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-25 17:29:20 +0000

    pendiente

---
+++ Clase4/Ejercisio20.java
@@ -0,0 +1,19 @@
+package Clase4;
+
+public class Ejercisio20 {
+    public int[][] llenarMatriz(int n,int m){
+        int[][] matriz = new int[n][m];
+        for(int i=0;i< matriz.length;i++){
+            for(int = j; j < matriz.length; i++){
+                matriz[i][j] = (int)(Math.random()*50+0)+0;
+            }
+        }
+        return matriz;
+    }
+    public String diagonalCerosMatriz(int[][]m){
+        int [][]matriz
+    }
+
+
+    
+}
