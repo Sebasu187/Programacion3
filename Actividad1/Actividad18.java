8dd50afc8ef4368adf95e7e3289bb74cc46da9bd
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-25 01:25:48 +0000

    imprimimos

--- Actividad1/Actividad18.java
+++ Actividad1/Actividad18.java
@@ -3,11 +3,16 @@ public class Actividad18 {
         int[][] tabla = new int[10][10];
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                tabla[i][j] = (i + 1) * (j + 1); 
+                tabla[i][j] = (i + 1) * (j + 1);
             }
         }
+        System.out.println("Tabla de multiplicar del 1 al 10:\n");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("%4d", tabla[i][j]); 
+            }
+            System.out.println(); 
+        }
+
     }
-
-
-    
 }
