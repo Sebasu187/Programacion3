9a6fcc24aa8c6d376445559b43457eee16b474f3
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-25 00:51:00 +0000

    leer datos del profesor

--- Actividad22/Ejercisio16.java
+++ Actividad22/Ejercisio16.java
@@ -1,18 +1,28 @@
+package Actividad22;
 import java.util.Scanner;
-public package Actividad22;
 
 class Ejercisio16 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese el número de profesores: ");
-        int n = scanner.nextInt();
-        scanner.nextLine();
+        int n = sc.nextInt();
+        sc.nextLine();
 
         String[] nombres = new String[n];
         int[] edades = new int[n];
         char[] sexos = new char[n];
 
-        
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nProfesor " + (i + 1) + ":");
+            System.out.print("Nombre: ");
+            nombres[i] = sc.nextLine();
+            System.out.print("Edad: ");
+            edades[i] = sc.nextInt();
+            System.out.print("Sexo (M/F): ");
+            sexos[i] = sc.next().charAt(0);
+            sc.nextLine(); // Limpiar el buffer
+        }
+
     }
 }
