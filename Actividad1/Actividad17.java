c10f01896baa655898f7cb749dc16330f1a88a96
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-25 01:18:32 +0000

    pedir dimensiones de la matriz

---
+++ Actividad1/Actividad17.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Actividad17 {
+    public static void main(String[] args) {
+        scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de filas: ");
+        int filas = scanner.nextInt();
+        System.out.print("Ingrese el número de columnas: ");
+        int columnas = scanner.nextInt();
+        
+    }
+    
+}
