13fb9943710348d8748ee82dc9e0ca80f4d92966
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-25 01:24:49 +0000

    llenamos la tabla matriz

--- Actividad1/Actividad18.java
+++ Actividad1/Actividad18.java
@@ -1,6 +1,11 @@
 public class Actividad18 {
     public static void main(String[] args) {
         int[][] tabla = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                tabla[i][j] = (i + 1) * (j + 1); 
+            }
+        }
     }
 
 
