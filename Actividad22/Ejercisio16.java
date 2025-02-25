d2328a7fed871f737d069a35ccc8934d2d2bf944
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-02-25 00:46:43 +0000

    pediumos el numero de profesores y cerramos el buffer

---
+++ Actividad22/Ejercisio16.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public package Actividad22;
+
+class Ejercisio16 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el número de profesores: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        
+    }
+}
