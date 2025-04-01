001cf6a20bea2702f708c5a803ae68ced5d53912
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-04-01 16:10:42 +0000

    Clase trabajador

---
+++ Clase9/Trabajador.java
@@ -0,0 +1,21 @@
+package Clase9;
+
+public class Trabajador {
+
+    private String nombre;
+    private String Cedula;
+    private double salario;
+
+    public Trabajador(String nombre, String cedula, double salario) {
+        this.nombre = nombre;
+        this.Cedula = cedula;
+        this.salario = salario;
+    }
+
+    public String getPagar() {
+        return "";
+    }
+
+
+
+}
