65ebe93676cb2c5cd4c75b02959ab1f8427d79cb
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-04-01 17:04:19 +0000

    Trabajadores

--- Clase9/Main.java
+++ Clase9/Main.java
@@ -2,20 +2,23 @@ package Clase9;
 
 import java.util.Stack;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Stack<Trabajador> trabajadores = new Stack<>();
 
-        Trabajador t1    = new Trabajador("Juan Perez", "001", 500.0);
-        Trabajador t2    = new Trabajador("Ana Gomez", "002", 600.0);
-        Trabajador t3    = new Trabajador("Carlos Ruiz", "003", 700.0);
+        Trabajador t1 = new Trabajador("Juan Perez", "001", 500.0);
+        Trabajador t2 = new Trabajador("Ana Gomez", "002", 600.0);
+        Trabajador t3 = new Trabajador("Carlos Ruiz", "003", 700.0);
 
         trabajadores.push(t1);
         trabajadores.push(t2);
         trabajadores.push(t3);
 
-        System.out.println(" Lista de Trabajadores:"+ trabajadores );
+        System.out.println(" Lista de Trabajadores:" + trabajadores);
 
+        for (int i = 0; i < trabajadores.size(); i++) {
+            System.out.println(trabajadores.elementAt(i).getNombre());// Muestra el trabajador en la cima de la pila
+        }
     }
 
 }
