3c4f85d645880b242391908b4a4db0e630ee514b
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-05-20 00:00:41 +0000

    administrador y autor

---
+++ Biblioteca/Autor.java
@@ -0,0 +1,21 @@
+public class Autor {
+    private String nombre;
+    private String nacionalidad;
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+}
